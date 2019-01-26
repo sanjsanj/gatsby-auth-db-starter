2c2408dd02d8d6b87e75b1f0d3dc4b438386e2ba
Sanjay Purswani <sanjsanj@hotmail.com>
2019-01-26 20:08:31 +0000

    First commit.  Setup Gatsby starter

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\dev\\gatsby-starter\\.cache\\dev-404-page.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\dev\\gatsby-starter\\src\\pages\\index.js")))
+}
+
